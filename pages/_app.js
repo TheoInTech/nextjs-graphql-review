4774c7456ff9ba02aab992dd626b5daf371cf071
Theo Roque <theoroque95@gmail.com>
2022-10-21 15:17:07 +0800

    reviewed nextjs and graphql integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Hydrate, QueryClientProvider } from "react-query";
+import { queryClient } from "../src/api";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
